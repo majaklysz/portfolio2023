8bbc41a29cfb78a60cd6fc687f3b459dc0ee27d8
maja <eaamkly@students.eaaa.dk>
2023-09-26 20:17:36 +0200

    now responsive for phone usage

--- js/projects.js
+++ js/projects.js
@@ -1,1 +1,10 @@
+document.querySelector("#menu").addEventListener("click", function (event) {
+  event.preventDefault();
+  var ml = document.querySelector(".menulist");
 
+  if (ml.classList.contains("active")) {
+    ml.classList.remove("active");
+  } else {
+    ml.classList.add("active");
+  }
+});
